b3b405626def10592ef036856edacb63d8179f72
FsaMudi <98121431+FsaMudi@users.noreply.github.com>
2022-01-20 22:43:33 +0200

    0.2 бета
    Добавлены команды

--- pythonius.py
+++ pythonius.py
@@ -7,9 +7,26 @@ while True:
   if a == 'Список':
     print('Твой крым?')
     print('Богдан?')
+    print('ИванЗоло')
+    print('IvanZolo')
+    print('Выйти')
+    print('______________')
   a = (input())
   if a == 'Твой крым?':
     print('dad why is my bolt')
   a = (input())
   if a == 'Богдан?':
     webbrowser.open('https://youtu.be/V8jodz8n-fQ', new=2)
+  a = (input())
+  if a == 'ИванЗоло':
+    webbrowser.open('https://www.tiktok.com/@ivanzolo2004/video/7051899013297196289?is_from_webapp=1&sender_device=pc&web_id7020771845147543045', new=1)
+    a = (input())
+  if a == 'Выйти':
+   exit()
+  a = (input())
+  if a == 'IvanZolo':
+    webbrowser.open('https://www.tiktok.com/@ivanzolo2004/video/7051899013297196289?is_from_webapp=1&sender_device=pc&web_id7020771845147543045', new=2)
+  a = (input())
+  if a == '':
+    webbrowser.open('https://www.tiktok.com/@ivanzolo2004/video/7051899013297196289?is_from_webapp=1&sender_device=pc&web_id7020771845147543045', new=2)
+    
