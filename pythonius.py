99f2fefdb986f1211f35fcb5ee2990a5794c2a85
FsaMudi <98121431+FsaMudi@users.noreply.github.com>
2022-01-20 21:42:32 +0200

    Bot alexei
    Исползование советуется прочитать

---
+++ pythonius.py
@@ -0,0 +1,15 @@
+from os import *
+
+import webbrowser
+print('Если команда не работает пишите её несколько раз все команды надо написать Список все команды с большой буквы')
+while True:
+  a = (input())
+  if a == 'Список':
+    print('Твой крым?')
+    print('Богдан?')
+  a = (input())
+  if a == 'Твой крым?':
+    print('dad why is my bolt')
+  a = (input())
+  if a == 'Богдан?':
+    webbrowser.open('https://youtu.be/V8jodz8n-fQ', new=2)
